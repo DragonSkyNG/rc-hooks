bd66b4c9d5dd272751d7b3725bbeb21d55551558
DragonSky_NG <dragonskyng@gmail.com>
2022-09-19 19:41:39 +0300

    Initial commit

---
+++ src/components/Login.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import axios from "axios";
+
+const Login = () => {
+  const [loginInfo, setLoginInfo] = useState({ username: "", password: "" });
+  const onChangeHandler = (e) => {
+    setLoginInfo((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+  const onClickHandler = async () => {
+    try {
+      console.log(loginInfo);
+      const res = await axios.post(
+        "http://localhost:3001/auth/login",
+        loginInfo
+      );
+      console.log(res);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <label htmlFor="username">Username:</label>
+      <input
+        onChange={onChangeHandler}
+        type="text"
+        id="username"
+        name="username"
+      />
+      <label htmlFor="password">Password:</label>
+      <input
+        onChange={onChangeHandler}
+        type="password"
+        id="password"
+        name="password"
+      />
+      <button onClick={onClickHandler}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/components/Users.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const Users = () => {
+  const [userData, setUserData] = useState([]);
+  useEffect(() => {
+    (async () => {
+      try {
+        const data = await axios.get("http://localhost:3001/person/getAll");
+        console.log(data);
+        setUserData(data);
+      } catch (error) {
+        console.error(error );
+      }
+    })();
+  }, []);
+  console.log(userData);
+  return <div></div>;
+};
+
+export default Users;
